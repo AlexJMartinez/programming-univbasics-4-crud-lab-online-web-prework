b093a915c370acaca17b900f9670cbdd50870052
Alex Martinez <alexandr3martin3z@gmail.com>
2019-09-24 01:30:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,8 +21,10 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  new_array = ["dog", "cat", "arrays!"]
-  arrays! = new_array.pop
+  new_array = ["dog", "cat", "fish"]
+  fish = new_array.pop
+  
+  
   
 
 end
